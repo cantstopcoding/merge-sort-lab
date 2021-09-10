1a3b1ef3d9ad23b45acd12529bc0ebaf9bdbb70d
cantstopcoding <jonathan_koss@yahoo.com>
2021-09-10 12:06:23 -0400

    create merge function

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function findMinAndRemoveSorted(arr) {
   return arr.shift();
 }
+
+function merge(subArr1, subArr2) {
+  let sorted = [];
+  while (!!subArr1.length && !!subArr2.length) {
+    if (subArr1[0] < subArr2[0]) {
+      sorted.push(findMinAndRemoveSorted(subArr1));
+    } else {
+      sorted.push(findMinAndRemoveSorted(subArr2));
+    }
+  }
+  return sorted.concat(subArr1).concat(subArr2);
+}
